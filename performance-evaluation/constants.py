5e272f2c180a8b632e3f8ee4fc664d1b0cd5e101
erosennin93 <julian.salas92@gmail.com>
2019-09-11 18:12:24 -0300

    identation fix

--- performance-evaluation/constants.py
+++ performance-evaluation/constants.py
@@ -62,7 +62,7 @@ folders_dictionary = {
         'parent_path': PERFORMANCE_STUDY_FOLDER_KEY
     },
     MANAGER_EVALUATION: {
-		'need_child_key': False,
+        'need_child_key': False,
         'parent_key': MANAGER_EVALUATION_FOLDER_KEY,
         'parent_path': PERFORMANCE_STUDY_FOLDER_KEY
     },
